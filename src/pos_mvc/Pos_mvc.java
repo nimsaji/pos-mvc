96b681046c26377d6537c571d7ea6b067e28131b
unknown <softech.series@gmail.com>
2023-08-31 22:55:30 +0530

    first commit

---
+++ src/pos_mvc/Pos_mvc.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pos_mvc;
+
+/**
+ *
+ * @author A-Z Electronic
+ */
+public class Pos_mvc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+      System.out.println("Hello java");
+    }
+    
+}
